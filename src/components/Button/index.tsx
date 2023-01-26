538b6bc4acd9ce5319e0b45ba14c480060be802c
thallesyam <thallesyam@gmail.com>
2023-01-26 18:32:13 -0300

    feat: create button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import { ButtonHTMLAttributes } from "react"
+import style from "./style.module.css"
+
+type IButton = ButtonHTMLAttributes<HTMLButtonElement> & {
+  text: string
+  icon?: JSX.Element
+}
+
+export function Button({ text, icon, ...props }: IButton) {
+  return (
+    <button
+      type="button"
+      {...props}
+      className={`${style.button} `.concat(props.className ?? "")}
+    >
+      <span>{text}</span>
+      {icon && icon}
+    </button>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,6 @@
+import { PlusCircle } from "phosphor-react"
+
+import { Button } from "@/components/Button"
 import { Header } from "@/components/Header"
 import { Input } from "@/components/Input"
 
@@ -9,7 +12,9 @@ export function Home() {
       <Header />
 
       <div className={style.searchContainer}>
-        <Input />
+        <Input name="search" />
+
+        <Button text="Criar" icon={<PlusCircle size={20} />} />
       </div>
     </div>
   )
