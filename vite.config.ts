a5fd19e5475d79db9face226ac6cf03e277138d2
thallesyam <thallesyam@gmail.com>
2023-01-26 17:22:10 -0300

    chore: add coverage vite deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,5 +12,9 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
   },
 })
